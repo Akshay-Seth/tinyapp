eb177764cdeb74eb74d776fb176d5063252276a9
BeamMeup-fail <51726921+BeamMeup-fail@users.noreply.github.com>
2021-06-07 14:16:16 -0700

    Created new urls and updated paths
    Updated new pathes so the server can now display short and long urls, and created the ejs for urls show

--- express_server.js
+++ express_server.js
@@ -24,6 +24,11 @@ app.get("/urls", (req, res) =>{
     const templateVars = { urls: urlDatabase}
     res.render("urls_index", templateVars);
 })
+app.get("/urls/:shortURL",(req, res) =>{
+    let shortURL = req.params.shortURL;
+    const templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL]};
+    res.render("urls_shows", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);

60c84e21f4821e8b0c7ef1e3bb94436c8c5e1ece
BeamMeup-fail <51726921+BeamMeup-fail@users.noreply.github.com>
2021-06-07 15:39:39 -0700

    Added new content to page
    upated the package.json for the parser! put in a new url_new. created a funtion to randomly generate numbers for the project and working on new port for urls

--- express_server.js
+++ express_server.js
@@ -2,6 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  length = 6;
+  let result = ' ';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
+}
+
+
+
 
 app.set("view engine", "ejs");
 
@@ -17,17 +34,34 @@ app.get("/", (req, res) => {
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
   });
+
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
   });
+
 app.get("/urls", (req, res) =>{
     const templateVars = { urls: urlDatabase}
     res.render("urls_index", templateVars);
-})
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_news");
+});
+
 app.get("/urls/:shortURL",(req, res) =>{
     let shortURL = req.params.shortURL;
     const templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL]};
     res.render("urls_shows", templateVars);
+});
+app.get("/u/:shortURL", (req, res) => {
+  let long = req.params.longURL;
+  const longURL = urlDatabase[long];
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
